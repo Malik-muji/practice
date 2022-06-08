5f3f62a28fac2ee53a7199c2ec76ee5a5ad66688
Malik-muji <106760406+Malik-muji@users.noreply.github.com>
2022-06-08 10:49:35 +0500

    Create insertData.php

--- insertData.php
+++ insertData.php
@@ -1,12 +1,4 @@
-<?php
-require 'connection.php';
-
-$u_name = $_POST['name'];
-$u_username = $_POST['user_name'];
-$u_email = $_POST['email'];
-$u_password = $_POST['password'];
-$u_gender = $_POST['gender'];
-
+<
 
 $date = date ("Y-m-d H:i:s");
 
@@ -15,14 +7,4 @@ $date = date ("Y-m-d H:i:s");
 $sqlQuery = "INSERT INTO user (name,user_name,email,password,gender,created_at) 
 VALUES ('$u_name','$u_username', '$u_email', '$u_password', '$u_gender', '$date')";
 
-if($con->query($sqlQuery)){
-echo'recorded';
-
-}else{
-    echo 'not recorded';
-}
-
-
-
-?>
-
+if($con->query($s
